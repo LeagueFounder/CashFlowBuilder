3533e70405bdc1177ce4f3c4895f053cdee2a62f
Vic Wintriss <sdvic@me.com>
2019-04-08 17:03:45 -0700

    Update ver 1.1

--- src/main/java/com/github/sdvic/ExtractPandLinfoForProjections.java
+++ src/main/java/com/github/sdvic/ExtractPandLinfoForProjections.java
@@ -1,5 +1,70 @@
 package com.github.sdvic;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import java.util.ArrayList;
+import static com.github.sdvic.Main.pandlSheet;
+
 public class ExtractPandLinfoForProjections
 {
+    public ArrayList<String> pandLAccountList = new ArrayList<String>();
+    public ArrayList<Integer> accountValues = new ArrayList<Integer>();
+    public Cell foundCell;
+
+    public void cashProjectionSetup()
+    {
+        pandLAccountList.add("43450 Individ, Business Contributions");
+        pandLAccountList.add("Total 47201 Tuition  Fees");
+        pandLAccountList.add("Total 47202 Workshop Fees");
+        pandLAccountList.add("Total 47204 Grant Scholarship");
+        pandLAccountList.add("62015 Salaries");
+        pandLAccountList.add("62041 Health Insurance");
+        pandLAccountList.add("62042 Workers Compensation Insurance");
+        pandLAccountList.add("62051 Social Security/Medicare");
+        pandLAccountList.add("62052 Federal Unemployment Tax");
+        pandLAccountList.add("62053 CA SUI");
+        pandLAccountList.add("62054 Telephone Reimbursement");
+        pandLAccountList.add("62055 Pension Expense");
+        pandLAccountList.add("62155 403(b) Plan Fees");
+        pandLAccountList.add("62850 Repairs and Maintenance");
+        pandLAccountList.add("62870 Property Insurance");
+        pandLAccountList.add("62890 Rent");
+        pandLAccountList.add("62891 Utilities");
+        pandLAccountList.add("62892 San Marcos School District");
+        pandLAccountList.add("Total 65000 Operations");
+        pandLAccountList.add("62110 Accounting Fees");
+        pandLAccountList.add("62155 403(b) Plan Fees");
+        pandLAccountList.add("62145 Payroll Service Fees");
+        pandLAccountList.add("62800 Facilities and Equipment");
+        pandLAccountList.add("65055 Breakroom Supplies");
+        pandLAccountList.add("Total 65100 Other Types of Expenses");
+        pandLAccountList.add("Total 68300 Travel and Meetings");
+        for (String s: pandLAccountList)
+        {
+            foundCell = findExcelTextEntry(pandlSheet, s);
+            int ri = foundCell.getRowIndex();
+            int pandlAccountValue = (int)pandlSheet.getRow(ri).getCell(1).getNumericCellValue();
+            accountValues.add(pandlAccountValue);
+            System.out.println(s + " => " + pandlAccountValue);
+        }
+    }
+    public static Cell findExcelTextEntry(Sheet sheet, String searchName)
+    {
+        for (Row row : sheet)
+        {
+            for (Cell cell : row)
+            {
+                if (cell.getCellType() == Cell.CELL_TYPE_STRING)
+                {
+                    if (cell.getStringCellValue().contains(searchName))
+                    {
+                        return cell;
+                    }
+                }
+            }
+        }
+        Cell cell = pandlSheet.getRow(0).getCell(0);
+        return cell;//error...can't find cell name
+    }
 }
--- src/main/java/com/github/sdvic/Setup.java
+++ src/main/java/com/github/sdvic/Setup.java
@@ -1,5 +1,80 @@
 package com.github.sdvic;
 
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.File;
+import java.io.FileInputStream;
+
+import static com.github.sdvic.Main.*;
+
 public class Setup
 {
+    public Setup()
+    {
+        System.out.println("========================== Setting Up =====================");
+        try
+        {
+            pandLfile = new File("/Users/VicMini/Desktop/PandL2018.xlsx");
+        }
+        catch (Exception e)
+        {
+            System.out.println("Can't make pandLfile");
+        }
+        try
+        {
+            sarahFiveYearFile = new File("/Users/VicMini/Desktop/sarah5yearPlan.xlsx");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Can't make sarahFiveYearFile" + e);
+        }
+        try
+        {
+            s5yrfis = new FileInputStream(sarahFiveYearFile);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Can't get s5yrfis " + e);//**********************
+        }
+        try
+        {
+            plfis = new FileInputStream(pandLfile);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Can't get plfis " + e);//**********************
+        }
+        try
+        {
+            pandlWorkbook = (XSSFWorkbook) WorkbookFactory.create(plfis);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Can't create pandLWorkbook");
+        }
+        try
+        {
+            pandlSheet = pandlWorkbook.getSheetAt(0);//P&L
+        }
+        catch (Exception e)
+        {
+            System.out.println("Can't get pandLSheet " + e);//**********************
+        }
+        try
+        {
+            sarah5yearWorkbook = (XSSFWorkbook) WorkbookFactory.create(s5yrfis);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Can't get sarah5yearWorkbook " + e);//**********************
+        }
+        try
+        {
+            sarah5yearSheet = sarah5yearWorkbook.getSheetAt(0);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Can't get sarah5yearSheet " + e);//**********************
+        }
+    }
 }
