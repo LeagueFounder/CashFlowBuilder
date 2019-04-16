432946c82289c78366f333631268d736295fd552
Vic Wintriss <sdvic@me.com>
2019-04-15 20:38:36 -0700

    New ver 1.2 extracting P%L and CashFlow

--- src/main/java/com/github/sdvic/Setup.java
+++ src/main/java/com/github/sdvic/Setup.java
@@ -1,80 +1,89 @@
 package com.github.sdvic;
 
-import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import java.io.File;
-import java.io.FileInputStream;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 
-import static com.github.sdvic.Main.*;
+import java.util.HashMap;
 
 public class Setup
 {
-    public Setup()
+    public HashMap<String, Integer> chartOfAccounts = new HashMap<>();
+    public Sheet pandlSheet;
+    public Sheet sarah5yearLocalSheet;
+
+    public Setup(Sheet pls, Sheet s5Ls)
     {
-        System.out.println("========================== Setting Up =====================");
-        try
+        System.out.println("Setting Up ==============");
+        String cellKey = null;
+        int cellValue = 0;
+        String title = null;
+        int amount = 0;
+        pandlSheet = pls;
+        sarah5yearLocalSheet = s5Ls;
+
+        System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& Reading Chart Of Accounts &&&&&&&&&&&&&&&&&&&&&&&&&");
+        for (Row row : pandlSheet)//Bring full chart of accounts from Excel (QuickBooks) P&L into HashMap chartOfAcocounts
         {
-            pandLfile = new File("/Users/VicMini/Desktop/PandL2018.xlsx");
+            if (row.getCell(1).getCellType() == Cell.CELL_TYPE_BLANK)
+            {
+                continue;
+            }
+            if (row.getCell(0).getCellType() == Cell.CELL_TYPE_STRING)
+            {
+                cellKey = row.getCell(0).getStringCellValue();
+                System.out.print(cellKey + "      ");
+            }
+            if (row.getCell(1).getCellType() == Cell.CELL_TYPE_NUMERIC)
+            {
+                cellValue = (int) row.getCell(1).getNumericCellValue();
+                System.out.println(cellValue);
+            }
+            chartOfAccounts.put(cellKey, cellValue);
         }
-        catch (Exception e)
+
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%% Cash Flow Analysis %%%%%%%%%%%%%%%%%%%%%%%");
+        for (Row row : sarah5yearLocalSheet)//Bring full cash flow chart from Excel
         {
-            System.out.println("Can't make pandLfile");
+            if (row.getCell(0).getCellType() == Cell.CELL_TYPE_BLANK)
+            {
+                continue;
+            }
+            if (row.getCell(0).getCellType() == Cell.CELL_TYPE_STRING)
+            {
+                title = row.getCell(0).getStringCellValue();
+                System.out.print(title + "         ");
+            }
+            if (row.getCell(1).getCellType() == Cell.CELL_TYPE_NUMERIC)
+            {
+                amount = (int) row.getCell(1).getNumericCellValue();
+                System.out.println(amount + "\n");
+            }
+            if (row.getCell(1).getCellType() == Cell.CELL_TYPE_BLANK)
+            {
+                continue;
+            }
+            System.out.println();
         }
-        try
-        {
-            sarahFiveYearFile = new File("/Users/VicMini/Desktop/sarah5yearPlan.xlsx");
-        }
-        catch(Exception e)
-        {
-            System.out.println("Can't make sarahFiveYearFile" + e);
-        }
-        try
-        {
-            s5yrfis = new FileInputStream(sarahFiveYearFile);
-        }
-        catch (Exception e)
-        {
-            System.out.println("Can't get s5yrfis " + e);//**********************
-        }
-        try
-        {
-            plfis = new FileInputStream(pandLfile);
-        }
-        catch (Exception e)
-        {
-            System.out.println("Can't get plfis " + e);//**********************
-        }
-        try
-        {
-            pandlWorkbook = (XSSFWorkbook) WorkbookFactory.create(plfis);
-        }
-        catch(Exception e)
-        {
-            System.out.println("Can't create pandLWorkbook");
-        }
-        try
-        {
-            pandlSheet = pandlWorkbook.getSheetAt(0);//P&L
-        }
-        catch (Exception e)
-        {
-            System.out.println("Can't get pandLSheet " + e);//**********************
-        }
-        try
-        {
-            sarah5yearWorkbook = (XSSFWorkbook) WorkbookFactory.create(s5yrfis);
-        }
-        catch (Exception e)
-        {
-            System.out.println("Can't get sarah5yearWorkbook " + e);//**********************
-        }
-        try
-        {
-            sarah5yearSheet = sarah5yearWorkbook.getSheetAt(0);
-        }
-        catch (Exception e)
-        {
-            System.out.println("Can't get sarah5yearSheet " + e);//**********************
+            System.out.println("Chart Of Accounts Loaded=========(" + chartOfAccounts.size() + " entries)");
+            System.out.println("Aggregating Cash Contributions****************");
+            Integer indBusContributions = chartOfAccounts.get("      43450 Individ, Business Contributions");
+            System.out.println("Individual, Business Contributions " + indBusContributions);
+            System.out.println("...............................................Total Cash Contributions => " + indBusContributions);
+            System.out.println("Aggregating Cash Grants******************");
+            Integer javaGrantTuition = chartOfAccounts.get("         47260 Java Tuition- Grant Scholarship");
+            System.out.println("Grant Java " + javaGrantTuition);
+            Integer grantWorkshops = chartOfAccounts.get("         47280 Java Workshop - Grant Scholarship");
+            System.out.println("Grant Workshops " + grantWorkshops);
+            int totalGrants = javaGrantTuition + grantWorkshops;
+            System.out.println("...............................................Total Cash Grants => " + totalGrants);
+            System.out.println("Aggregating Cash Tuition*********************");
+            Integer javaTuition = chartOfAccounts.get("      Total 47201 Tuition  Fees");
+            System.out.println("Java Tuition " + javaTuition);
+            Integer workshopTuition = chartOfAccounts.get("      Total 47202 Workshop Fees");
+            System.out.println("Workshop Tuition " + workshopTuition);
+            int totalTuition = javaTuition + workshopTuition;
+            System.out.println("...............................................Total Cash Tuition => " + totalTuition);
+            System.out.println("TOTAL CASH INCOME => " + (totalGrants + totalTuition));
         }
     }
-}
