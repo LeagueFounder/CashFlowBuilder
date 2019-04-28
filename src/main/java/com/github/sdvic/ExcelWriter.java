8819736a641efbdb3aff8d37a0fb006350903088
Vic Wintriss <sdvic@me.com>
2019-04-27 20:34:40 -0700

    ver 1.6 Reading p&l and cash Flow OK

--- src/main/java/com/github/sdvic/ExcelWriter.java
+++ src/main/java/com/github/sdvic/ExcelWriter.java
@@ -1,15 +1,17 @@
 package com.github.sdvic;
 /****************************************************************************************
  *  * Application to extract Cash Flow data from Quick Books P&L and build Cash Projections
- * rev 1.4 April 17, 2019
+ * rev 1.6 April 27, 2019
  * copyright 2019 Vic Wintriss
  ****************************************************************************************/
 import org.apache.poi.ss.usermodel.Sheet;
 
+import java.io.FileOutputStream;
+
 public class ExcelWriter
 {
-    public ExcelWriter(Sheet s5ls)
+    public ExcelWriter(FileOutputStream s5yrfos)
     {
-
+    //s5yrfos.write();
     }
 }
--- src/main/java/com/github/sdvic/Main.java
+++ src/main/java/com/github/sdvic/Main.java
@@ -1,12 +1,13 @@
 package com.github.sdvic;
 /****************************************************************************************
  *  * Application to extract Cash Flow data from Quick Books P&L and build Cash Projections
- * rev 1.4 April 17, 2019
+ * rev 1.6 April 27, 2019
  * copyright 2019 Vic Wintriss 
  ****************************************************************************************/
 
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFFormulaEvaluator;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import javax.swing.*;
@@ -14,27 +15,21 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
-/*******************************************************************************
- *   Extract P&L data from QuickBooks, analyze and insert into a cash flow chart
- *   ver 1.3 April 17, 2019
- *******************************************************************************/
 public class Main implements Runnable
 {
-    public  XSSFWorkbook pandlWorkbook;//P&L
-    public  XSSFWorkbook sarah5yearLocalWorkbook;//5Year
-    public  Sheet pandlSheet;//P&L
-    public  Sheet sarah5yearLocalSheet;//5Year
     public  File pandLfile;
-    public  File sarah5YearRemoteFile;
     public  FileInputStream plfis;
+    public  XSSFWorkbook pandlWorkbook;//P&L
+    public  Sheet pandlSheet;//P&L
+
+    public  File sarah5YearFile;
     public  FileInputStream s5yrfis;
-    public  FileOutputStream plfos;
-    public  FileOutputStream s5yrfos;
+    public  XSSFWorkbook sarah5yearWorkbook;//5Year
+    public  Sheet sarah5yearSheet;//5Year
 
     public static void main(String[] args)
     {
         System.out.println("Cash Flow Generator ver 1.2 4/15/19");
-
         SwingUtilities.invokeLater(new Main());
     }
 
@@ -46,17 +41,18 @@ public class Main implements Runnable
             plfis = new FileInputStream(pandLfile);
             pandlWorkbook = (XSSFWorkbook) WorkbookFactory.create(plfis);
             pandlSheet = pandlWorkbook.getSheetAt(0);//P&L
-            sarah5YearRemoteFile = new File("/Users/VicMini/Desktop/SarahFiveYearPlan.xlsx");
-            s5yrfis = new FileInputStream(sarah5YearRemoteFile);
-            sarah5yearLocalWorkbook = (XSSFWorkbook) WorkbookFactory.create(s5yrfis);
-            sarah5yearLocalSheet = sarah5yearLocalWorkbook.getSheetAt(0);
+
+            sarah5YearFile = new File("/Users/VicMini/Desktop/SarahFiveYearPlan.xlsx");
+            s5yrfis = new FileInputStream(sarah5YearFile);
+            sarah5yearWorkbook = (XSSFWorkbook) WorkbookFactory.create(s5yrfis);
+            sarah5yearSheet = sarah5yearWorkbook.getSheetAt(0);
+            new ExcelReader(pandlSheet, sarah5yearSheet, sarah5yearWorkbook);
         }
         catch (Exception e)
         {
             System.out.println("Exception in setup()" + e);
         }
-        new ExcelReader(pandlSheet, sarah5yearLocalSheet);
-        new ExcelWriter(sarah5yearLocalSheet);
+
     }
 }
 
