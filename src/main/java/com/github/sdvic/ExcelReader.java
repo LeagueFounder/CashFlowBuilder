4e4ac11a601e34aafdb69c3516b70e00bb472bcc
Vic Wintriss <sdvic@me.com>
2019-04-27 20:30:58 -0700

    ver 1.6 Reading p&l OK and cash Flow OK

--- src/main/java/com/github/sdvic/ExcelReader.java
+++ src/main/java/com/github/sdvic/ExcelReader.java
@@ -1,71 +1,127 @@
 package com.github.sdvic;
 /****************************************************************************************
  *  * Application to extract Cash Flow data from Quick Books P&L and build Cash Projections
- * rev 1.4 April 17, 2019
+ * rev 1.6 April 27, 2019
  * copyright 2019 Vic Wintriss
  ****************************************************************************************/
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ExcelReader
 {
-    public HashMap<String, Integer> chartOfAccounts = new HashMap<>();
+    public HashMap<String, Integer> chartOfAccountsMap = new HashMap<>();
     public Sheet pandlSheet;
-    public Sheet sarah5yearLocalSheet;
+    public Sheet sarah5yearSheet;
+    public FormulaEvaluator evaluator;
 
-    public ExcelReader(Sheet pls, Sheet s5Ls)
+    public ExcelReader(Sheet pls, Sheet s5Ls, Workbook s5yrwb)
     {
         System.out.println("Setting Up ==============");
         String cellKey = null;
         int cellValue = 0;
-        String title = null;
-        int amount = 0;
         pandlSheet = pls;
-        sarah5yearLocalSheet = s5Ls;
-        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%% Cash Flow Analysis %%%%%%%%%%%%%%%%%%%%%%%");
-        for (Row row : sarah5yearLocalSheet)//Bring full cash flow chart from Excel
-        {
-            if (row.getCell(0).getCellType() == Cell.CELL_TYPE_BLANK)
-            {
-                continue;
-            }
-            if (row.getCell(0).getCellType() == Cell.CELL_TYPE_STRING)
-            {
-                title = row.getCell(0).getStringCellValue();
-                System.out.print(title + "         ");
-            }
-            if (row.getCell(1).getCellType() == Cell.CELL_TYPE_NUMERIC)
-            {
-                amount = (int) row.getCell(1).getNumericCellValue();
-                System.out.println(amount + "\n");
-            }
-            if (row.getCell(1).getCellType() == Cell.CELL_TYPE_BLANK)
-            {
-                continue;
-            }
-            System.out.println();
-        }
+        sarah5yearSheet = s5Ls;
+        evaluator = s5yrwb.getCreationHelper().createFormulaEvaluator();
+
         System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& Reading Chart Of Accounts &&&&&&&&&&&&&&&&&&&&&&&&&");
-        for (Row row : pandlSheet)//Bring full chart of accounts from Excel (QuickBooks) P&L into HashMap chartOfAcocounts
+        for (Row row : pls)//Bring full chart of accounts from Excel (QuickBooks) P&L into HashMap chartOfAcocounts
         {
-            if (row.getCell(1).getCellType() == Cell.CELL_TYPE_BLANK)
+            if (isRowEmpty(row))
             {
                 continue;
             }
-            if (row.getCell(0).getCellType() == Cell.CELL_TYPE_STRING)
+            else
             {
-                cellKey = row.getCell(0).getStringCellValue();
-                System.out.print(cellKey + "      ");
+                for (Cell cell : row)
+                {
+                    if (cell != null)
+                    {
+                        evaluator.evaluateAll();
+                        switch (cell.getCellType())
+                        {
+                            case XSSFCell.CELL_TYPE_BLANK:
+                            case XSSFCell.CELL_TYPE_BOOLEAN:
+                                break;
+                            case XSSFCell.CELL_TYPE_FORMULA:
+                                System.out.print((int)cell.getNumericCellValue() + "   ");
+                                break;
+                            case XSSFCell.CELL_TYPE_NUMERIC:
+                                System.out.print((int)cell.getNumericCellValue() + "\t\t\t\t");
+                                break;
+                            case XSSFCell.CELL_TYPE_STRING:
+                                System.out.print(cell + "\t");
+                                break;
+                        }
+                        System.out.print("\t");
+                    }
+                }
+                System.out.println();
             }
-            if (row.getCell(1).getCellType() == Cell.CELL_TYPE_NUMERIC)
-            {
-                cellValue = (int) row.getCell(1).getNumericCellValue();
-                System.out.println(cellValue);
-            }
-            chartOfAccounts.put(cellKey, cellValue);
+            chartOfAccountsMap.put(cellKey, cellValue);
         }
+        System.out.println("Chart Of Acclounts HashMap size => " + chartOfAccountsMap.size());
+        for (Map.Entry<String, Integer> pair : chartOfAccountsMap.entrySet())
+        {
+            System.out.println(pair.getKey() + "   " + pair.getValue());
+
+        }
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%% Cash Flow Analysis %%%%%%%%%%%%%%%%%%%%%%%");
+        evaluator.evaluateAll();
+        for (Row row : sarah5yearSheet)//Bring full cash flow chart from Excel
+        {
+            if (isRowEmpty(row))
+            {
+                continue;
+            }
+            else
+            {
+                for (Cell cell : row)
+                {
+                    if (cell != null)
+                    {
+                        switch (cell.getCellType())
+                        {
+                            case XSSFCell.CELL_TYPE_BLANK:
+                            case XSSFCell.CELL_TYPE_BOOLEAN:
+                                break;
+                            case XSSFCell.CELL_TYPE_FORMULA:
+                                System.out.print((int)cell.getNumericCellValue() + "   ");
+                                break;
+                            case XSSFCell.CELL_TYPE_NUMERIC:
+                                System.out.print((int)cell.getNumericCellValue() + "   ");
+                                break;
+                            case XSSFCell.CELL_TYPE_STRING:
+                                System.out.print(cell + "\t");
+                                break;
+                        }
+                        System.out.print("\t");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    public static boolean isRowEmpty(Row row)//From StackOverflow
+    {
+        boolean isEmpty = true;
+        DataFormatter dataFormatter = new DataFormatter();
+        if (row != null)
+        {
+
+            for (Cell cell : row)
+            {
+                if (dataFormatter.formatCellValue(cell).trim().length() > 0)
+                {
+                    isEmpty = false;
+                    break;
+                }
+            }
+        }
+        return isEmpty;
     }
 }
